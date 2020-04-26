e04f40e283e008e9bef666c5830d15d9f70ea760
pkrisko <patrick.krisko@gmail.com>
2020-04-26 14:56:42 -0500

    add gh-pages dependency, show genres as list

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,30 @@ const ArtistId = () => {
   const [artistId, setArtistId] = useState('5TBdr1d9ZJ5YMhsxiF1Jo7');
   const [popularity, setPopularity] = useState('');
   const [src, setSrc] = useState('');
+  const [genres, setGenres] = useState([]);
   const onChange = e => setArtistId(e.target.value);
   const onSubmit = async e => {
     e.preventDefault();
     const artist = await getArtistById(artistId);
+    const { popularity, images, genres } = artist;
     if (artist) {
-      setPopularity(artist.popularity);
-      setSrc(artist.images[1].url)
+      setPopularity(popularity);
+      setSrc(images[2].url);
+      setGenres(genres);
     }
   }
   return <>
     <form onSubmit={onSubmit}>
       <label htmlFor="artistId">
-        <p>Enter Arist Id</p>
+        <p>Enter Artist Id</p>
         <input type="text" name="artistId" value={artistId} onChange={onChange} />
         <input type="submit" value="Go" />
         <p>Popularity: {popularity}</p>
         {src.length > 0 && <p><img src={src} /></p>}
+        {genres.length > 0 && <p>Genres: </p>}
+        {genres.length > 0 && genres.map(genre => (
+          <div>{genre}</div>
+        ))}
       </label>
     </form>
   </>
