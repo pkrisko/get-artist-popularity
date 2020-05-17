604254149bd4c920691210bce092fb2234be168b
pkrisko <patrick.krisko@gmail.com>
2020-05-17 11:55:11 -0500

    Add changes from working session with zach

--- client/src/components/ArtistInfoById.js
+++ client/src/components/ArtistInfoById.js
@@ -2,44 +2,46 @@ import React, { useState } from 'react';
 import { getArtistById } from '../services/getArtist';
 import ArtistInfoCard from './ArtistInfoCard';
 
-const parseUri = uri => {
-  let parsed = [];
-  if (!uri || typeof uri !== 'string') return;
-  parsed = uri.split(':');
-  if (parsed.length !== 3) return;
-  return parsed[2];
+const parseUri = link => {
+  if (link.includes("spotify:artist:")) { // URI
+    return link.substring(15);
+  } else if (link.includes("https://open.spotify.com")) { // Artist Link
+    return link.substring(32, 54);
+  }
 }
 
 const ArtistInfoById = () => {
-    const [artistId, setArtistId] = useState('5TBdr1d9ZJ5YMhsxiF1Jo7');
-    const [artist, setArtist] = useState(null);
-    const onChange = e => setArtistId(e.target.value);
-    const onSubmit = async e => {
-      e.preventDefault();
-      let artistIdParsed = artistId.includes(':')
-        ? parseUri(artistId)
-        : artistId;
-      const res = await getArtistById(artistIdParsed);
-      res && setArtist(res);
-    }
-    return (
-      <>
-        <form onSubmit={onSubmit}>
-          <label htmlFor="artistId">
-            <p>Enter Artist Id or URI</p>
-            <input type="text" id="artist-id" name="artistId" value={artistId} onChange={onChange} />
-          </label>
-          <input type="submit" id="submit" value="Go" />
-        </form>
-        {artist && <ArtistInfoCard
-          popularity={artist.popularity}
-          imgSrc={artist.images[0].url}
-          genres={artist.genres}
-          name={artist.name}
-          followers={artist.followers.total}
-        />}
-      </>
-    );
+  const [artistId, setArtistId] = useState('https://open.spotify.com/artist/5TBdr1d9ZJ5YMhsxiF1Jo7?si=TO2BN_n9QZyOpuw9B4fLQQ');
+  const [artist, setArtist] = useState(null);
+  const onChange = e => setArtistId(e.target.value);
+  const onSubmit = async e => {
+    e.preventDefault();
+    let artistIdParsed = artistId.includes(':')
+      ? parseUri(artistId)
+      : artistId;
+    const res = await getArtistById(artistIdParsed);
+    res && setArtist(res);
   }
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="artistId">
+          <div id="header-area">
+            <span id="title">Enter Artist Link or URI</span>
+          </div>
+          <input type="text" id="artist-id" name="artistId" value={artistId} onChange={onChange} />
+        </label>
+        <input type="submit" id="submit" value="Go" />
+      </form>
+      {artist && <ArtistInfoCard
+        popularity={artist.popularity}
+        imgSrc={artist.images[0].url}
+        genres={artist.genres}
+        name={artist.name}
+        followers={artist.followers.total}
+      />}
+    </>
+  );
+}
 
 export default ArtistInfoById;
